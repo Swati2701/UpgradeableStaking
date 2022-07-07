52f76287710398537be3ff86405ab288eee12d0c
abc <swati@solulab.com>
2022-07-07 12:56:29 +0530

    argument file for staking contract

---
+++ argument.js
@@ -0,0 +1,8 @@
+module.exports = [
+  "0x8BEb6e342A755aC94514c0F6b29b9b1542B7DC44", //token address
+  "0x2b57dCE851856B3C1B91e98AaEBBF60B5bb4B411", //fetch price address
+];
+
+//ERC20 Token Deployed to: 0x8BEb6e342A755aC94514c0F6b29b9b1542B7DC44
+// Fetch Price contract deployed to: 0x2b57dCE851856B3C1B91e98AaEBBF60B5bb4B411
+// upgradeable staking contract deployed to: 0xf24f169fE55B98aD46F56B6e903F32ff36531dD2
